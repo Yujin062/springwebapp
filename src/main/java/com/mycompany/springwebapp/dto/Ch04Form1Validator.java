035e74e9e0998d48a872f9e27ae7e12aa2bb5ac6
yuyu062 <chorong6544@naver.com>
2023-07-21 10:47:46 +0900

    param1 검사

--- src/main/java/com/mycompany/springwebapp/dto/Ch04Form1Validator.java
+++ src/main/java/com/mycompany/springwebapp/dto/Ch04Form1Validator.java
@@ -16,6 +16,16 @@ public class Ch04Form1Validator implements Validator{
 	@Override
 	public void validate(Object target, Errors errors) {
 		log.info("실행");
+		Ch04Form1 ch04Form1 = (Ch04Form1) target;
 		
+		//param1 검사
+		String param1 = ch04Form1.getParam1();
+		if(param1 == null || param1.equals("")) {
+			errors.rejectValue("param1", "errors.form1.param1.required", "필수 입력");
+		}else if(param1.length() < 8){
+			errors.rejectValue("param1", "errors.form1.param1.minlength", "최소 8자 입력");	
+		}else if(param1.length() > 15) {
+			errors.rejectValue("param1", "errors.form1.param1.maxlength", "최대 15자 입력");			
+		}
 	}
 }
