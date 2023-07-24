03da016fed90a05a1d95e19f073391ab8a915426
yuyu062 <chorong6544@naver.com>
2023-07-24 15:46:33 +0900

    ch08 추가

--- src/main/java/com/mycompany/springwebapp/controller/Ch08Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch08Controller.java
@@ -1,19 +1,15 @@
 package com.mycompany.springwebapp.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.mycompany.springwebapp.dto.Ch07Board;
+import com.mycompany.springwebapp.dto.Ch08Item;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,5 +20,30 @@ public class Ch08Controller {
 	@RequestMapping("/content")
 	public String content() {
 		return "ch08/content";
+	}
+	
+	@PostMapping("/addCart")
+	public String addCart(Ch08Item item, HttpSession session){
+		//세션에서 카트 가져오기
+		List<Ch08Item> cart = (List<Ch08Item>) session.getAttribute("cart");
+		//세션에 카트가 없을 경우, 새로 생성해서 카트에 저장
+		if(cart == null) {
+			cart = new ArrayList<Ch08Item>();
+			session.setAttribute("cart", cart);
+		}
+		//카트에 해당 아이템이 있는지 조사
+		boolean exist = false;
+		for(Ch08Item cartItem: cart) {
+			if(cartItem.getName().equals(item.getName())) {
+				//카트에 기존아이템이 있을 경우, 양만 수정
+				cartItem.setAmount(cartItem.getAmount() + item.getAmount());
+				exist = true;
+			}
+		}
+		//카트에 없는 새로운 아이탬일 경우
+		if(exist == false) {
+			cart.add(item);
+		}
+		return "redirect:/ch08/content";
 	}
 }
--- src/main/webapp/WEB-INF/views/ch08/content.jsp
+++ src/main/webapp/WEB-INF/views/ch08/content.jsp
@@ -1,22 +1,41 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<%-- 
-1) header.jsp의 소스 코드를 복사해서 붙여넣기 
-2) 절대경로/는 웹애플리케이션의 로컬루트(WebContent 폴더)
---%>
 <%@ include file="/WEB-INF/views/common/header.jsp" %>
 
 <%-- index.jsp의 중앙 내용 --%>
 <div class="card m-2">
    <div class="card-header">
-      Home
+       	세션 이용
    </div>
    <div class="card-body">
+   	<form method="post" action="addCart">
+   		<div class="form-group"> 
+	   		<label for="name">상품선택: </label>
+	   		<select class="form-control" id="name" name="name">
+	   			<option value="item1">아이템 1</option>
+	   			<option value="item2">아이템 2</option>
+	   			<option value="item3">아이템 3</option>
+	   			<option value="item4">아이템 4</option>
+	   			<option value="item5">아이템 5</option>
+	   		</select>
+   		</div>
+   		<div class="form-group">
+   			<label for="ammound">수량:</label>
+	   		<input type="number" name="amount" value="1">
+   		</div>
+   		<button type="submit" class="btn btn-info btn-sm mt-2">장바구니 넣기</button>
+   	</form>
+   	<a href="clearCart" class="btn btn-info btn-sm mt-2">장바구니 비우기</a>
+   	
+   	<hr/>
+   	<p>장바구니 내용</p>
+   	<ul>
+   		<c:forEach var="item" items="${cart}">
+   			<li>${item.name}, ${item.amount}개</li>
+   		</c:forEach>
+   	</ul>
    </div>
 </div>
 
-<%-- 
-1) footer.jsp의 소스 코드를 복사해서 붙여넣기 
-2) 절대경로/는 웹애플리케이션의 로컬루트(WebContent 폴더)
---%>
 <%@ include file="/WEB-INF/views/common/footer.jsp" %>
