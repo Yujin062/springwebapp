38ca0d54d8934cd14f8a762089a680d13f91c12d
yuyu062 <chorong6544@naver.com>
2023-07-25 11:33:12 +0900

    ajax로 파일 업로드

--- src/main/java/com/mycompany/springwebapp/controller/Ch09Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch09Controller.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mycompany.springwebapp.dto.Ch09FileUpload;
 
@@ -56,5 +58,26 @@ public class Ch09Controller {
 		fileUpload.getAttach().transferTo(file);
 		
 		return "redirect:/ch09/content";
+	}
+	@PostMapping(value="/fileuploadAjax", produces="application/json; charset=UTF-8")
+	@ResponseBody
+	public String fileuploadAjax(Ch09FileUpload fileUpload) throws Exception{
+		log.info("title: " + fileUpload.getTitle());
+		log.info("desc: " + fileUpload.getDesc());
+		log.info("originalFilename: " + fileUpload.getAttach().getOriginalFilename());
+		log.info("contentType: " + fileUpload.getAttach().getContentType());
+		log.info("file size: " + fileUpload.getAttach().getSize());
+		
+		//받은 파일을 영구적으로 저장하기
+		//실제 서버에 저장되는 파일 이름(중복이 되면 안됌)
+		String saveFilename = new Date().getTime() + "-" + fileUpload.getAttach().getOriginalFilename();
+		String saveFilepath = "C:/OTI/uploadfiles/" + saveFilename;
+		File file = new File(saveFilepath);
+		fileUpload.getAttach().transferTo(file);
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("result", "success");
+		String json = jsonObject.toString();
+		return json;
 	}
 }
