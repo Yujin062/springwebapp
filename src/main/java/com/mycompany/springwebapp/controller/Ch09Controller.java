2ceb980f14865f390e17ae8f4b1abbc2b4f14e91
yuyu062 <chorong6544@naver.com>
2023-07-25 11:21:40 +0900

    dto객체로 파일 저장

--- src/main/java/com/mycompany/springwebapp/controller/Ch09Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch09Controller.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mycompany.springwebapp.dto.Ch09FileUpload;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -21,7 +23,7 @@ public class Ch09Controller {
 		return "ch09/content";
 	}
 	
-	@PostMapping("/fileupload")
+	/*@PostMapping("/fileupload")
 	public String fileupload(String title, String desc, MultipartFile attach) throws Exception{
 		log.info("title: " + title);
 		log.info("desc: " + desc);
@@ -35,6 +37,23 @@ public class Ch09Controller {
 		String saveFilepath = "C:/OTI/uploadfiles/" + saveFilename;
 		File file = new File(saveFilepath);
 		attach.transferTo(file);
+		
+		return "redirect:/ch09/content";
+	}*/
+	@PostMapping("/fileupload")
+	public String fileupload(Ch09FileUpload fileUpload) throws Exception{
+		log.info("title: " + fileUpload.getTitle());
+		log.info("desc: " + fileUpload.getDesc());
+		log.info("originalFilename: " + fileUpload.getAttach().getOriginalFilename());
+		log.info("contentType: " + fileUpload.getAttach().getContentType());
+		log.info("file size: " + fileUpload.getAttach().getSize());
+		
+		//받은 파일을 영구적으로 저장하기
+		//실제 서버에 저장되는 파일 이름(중복이 되면 안됌)
+		String saveFilename = new Date().getTime() + "-" + fileUpload.getAttach().getOriginalFilename();
+		String saveFilepath = "C:/OTI/uploadfiles/" + saveFilename;
+		File file = new File(saveFilepath);
+		fileUpload.getAttach().transferTo(file);
 		
 		return "redirect:/ch09/content";
 	}
