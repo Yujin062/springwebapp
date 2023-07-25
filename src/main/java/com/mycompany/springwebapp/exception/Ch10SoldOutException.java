68dd669154e30da9b67095da3645d13ebd9479a0
yuyu062 <chorong6544@naver.com>
2023-07-25 15:30:46 +0900

    Ch10OutException 예외처리 클래스

---
+++ src/main/java/com/mycompany/springwebapp/exception/Ch10SoldOutException.java
@@ -0,0 +1,17 @@
+package com.mycompany.springwebapp.exception;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@ControllerAdvice
+@Slf4j
+public class Ch10SoldOutException extends RuntimeException{
+	public Ch10SoldOutException() {
+	}
+	public Ch10SoldOutException(String message) {
+		super(message);
+	}
+}
