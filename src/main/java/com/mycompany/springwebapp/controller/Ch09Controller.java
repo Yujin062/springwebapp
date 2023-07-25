3b0c6874c85c5d207fa6adb565647ee806931f51
yuyu062 <chorong6544@naver.com>
2023-07-25 10:36:06 +0900

    ch09 controller fileupload 메소드 추가

--- src/main/java/com/mycompany/springwebapp/controller/Ch09Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch09Controller.java
@@ -1,19 +1,11 @@
 package com.mycompany.springwebapp.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import com.mycompany.springwebapp.dto.Ch08Item;
-import com.mycompany.springwebapp.dto.Ch08Member;
-import com.mycompany.springwebapp.interceptor.Login;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,4 +18,14 @@ public class Ch09Controller {
 		return "ch09/content";
 	}
 	
+	@PostMapping("/fileupload")
+	public String fileupload(String title, String desc, MultipartFile attach) {;
+		log.info("title: " + title);
+		log.info("desc: " + desc);
+		log.info("originalFilename: " + attach.getOriginalFilename());
+		log.info("contentType: " + attach.getContentType());
+		log.info("file size: " + attach.getSize());
+		
+		return "redirect:/ch09/content";
+	}
 }
