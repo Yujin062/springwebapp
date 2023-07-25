130c702db7888e2d9ac612a858d1254d2707586a
yuyu062 <chorong6544@naver.com>
2023-07-25 16:21:47 +0900

    Ch11Controller, content 추가

---
+++ src/main/java/com/mycompany/springwebapp/controller/Ch11Controller.java
@@ -0,0 +1,23 @@
+package com.mycompany.springwebapp.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.springwebapp.exception.Ch10SoldOutException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/ch10")
+public class Ch11Controller {
+   
+   @RequestMapping("/content")
+   public String content() {
+      return "ch10/content";
+   }
+   
+}
