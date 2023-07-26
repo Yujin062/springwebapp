cf71c38cd5002ac683b970b31b5c236121cdff9f
yuyu062 <chorong6544@naver.com>
2023-07-26 17:00:59 +0900

    xml을 이용한 주입

--- src/main/java/com/mycompany/springwebapp/service/Ch12ServiceDiByXml.java
+++ src/main/java/com/mycompany/springwebapp/service/Ch12ServiceDiByXml.java
@@ -1,8 +1,9 @@
 package com.mycompany.springwebapp.service;
 
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import com.mycompany.springwebapp.dao.Ch12DaoByXml1;
 import com.mycompany.springwebapp.dao.Ch12DaoByXml2;
@@ -17,11 +18,13 @@ public class Ch12ServiceDiByXml {
 	private Ch12DaoByXml2 daoxml2;
 	private Ch12DaoByXml3 daoxml3;
 	//생성자
-	public Ch12ServiceDiByXml() {}
+	public Ch12ServiceDiByXml() {
+		log.info("실행1");
+	}
 	//생성자
-	public Ch12ServiceDiByXml(Ch12DaoByXml2 daoByXml2) {
-		log.info("실행");
-		this.daoxml2 = daoByXml2;
+	public Ch12ServiceDiByXml(Ch12DaoByXml1 daoByXml1) {
+		log.info("실행2");
+		this.daoxml1 = daoByXml1;
 	}
 	
 	//setter
@@ -36,6 +39,24 @@ public class Ch12ServiceDiByXml {
 	public void setCh12DaoByXml3(Ch12DaoByXml3 daoByXml3) {
 		log.info("실행");
 		this.daoxml3 = daoByXml3;
+	}
+	public void setCollection1(List<String> list) {
+		log.info("실행");
+		for(String item: list) {
+			log.info(item);			
+		}
+	}
+	public void setCollection2(Set set) {
+		log.info("실행");
+		log.info("아이템 수: " + set.size());
+	}
+	public void setCollection3(Map<String, Object>map) {
+		log.info("실행");
+		log.info("아이템 수: " + map.size());
+	}
+	public void setCollection4(Properties prop) {
+		log.info("실행");
+		log.info("아이템 수: " + prop.size());
 	}
 	
 	//인스턴스 메소드
