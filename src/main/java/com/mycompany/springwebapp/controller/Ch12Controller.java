5917e97e7a3f6419d74d4aa7e6e87dd14e8d23ed
yuyu062 <chorong6544@naver.com>
2023-07-26 17:58:48 +0900

    Annotation을 이용한 주입

--- src/main/java/com/mycompany/springwebapp/controller/Ch12Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch12Controller.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mycompany.springwebapp.dao.Ch12DaoByXml1;
+import com.mycompany.springwebapp.service.Ch12ServiceDiByAnnotation;
 import com.mycompany.springwebapp.service.Ch12ServiceDiByXml;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +18,14 @@ public class Ch12Controller {
    @Autowired
    private Ch12ServiceDiByXml serviceDiByXml;
    
+   @Autowired
+   private Ch12ServiceDiByAnnotation serviceDiByAnnotation;
+   
    @RequestMapping("/content")
    public String content(HttpSession session) {
       serviceDiByXml.method();
-       
+      serviceDiByAnnotation.method(); 
 	   return "ch12/content";
    }
-   
-  
+ 
 }
